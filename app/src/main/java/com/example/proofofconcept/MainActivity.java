84ba3aaec814763b22cf2deafc026d40cf99b43e
Sparsh Kumar <S.Kumar10@ncl.ac.uk>
2024-10-24 17:51:42 +0100

    Implemented the main activity of the application, this is the entry point of the application. - Set up a text view to display as the main page of the app along with the text. - Set up a button with an onClickListener. On click, increases counter by 1 and logs the activity in logcat. as counter reaches 10, it sends an intent to the application to open a link in the browser.
    Also implemented a secret activity to handle incoming SEND intents of the type text/MIME type.
    - Displayed the shared text from other applications in the textview.
    
    Lastly, set up intent filters in AndroidManifest.xml to allow external sharing of plain text to the app.

---
+++ app/src/main/java/com/example/proofofconcept/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.proofofconcept;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    int counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        //adds a text view to the application
+            TextView homeText = findViewById(R.id.home_text);
+        homeText.setText("This is My Exploiting Application");
+
+        /* adds a button and then we have a lambda function which I don't actually
+         know the purpose of but it increases the count by 1*/
+        Button homeButton = findViewById(R.id.home_button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                // logs the clicks made by the user
+                Log.i("HEXTREE", "The Button has been clicked "+counter+" times.");
+                homeText.setText("Clicked: "+counter);
+
+                /* if the count reaches 10, it sends an intent that parses/searches the web for the provided link
+                 and opens it on the user's default browser*/
+                if (counter==10) {
+                    Intent browserIntent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://www.google.com/"));
+                    startActivity(browserIntent);
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/proofofconcept/SecretActivity.java
@@ -0,0 +1,28 @@
+    package com.example.proofofconcept;
+
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.widget.TextView;
+
+    import androidx.activity.EdgeToEdge;
+    import androidx.appcompat.app.AppCompatActivity;
+    import androidx.core.graphics.Insets;
+    import androidx.core.view.ViewCompat;
+    import androidx.core.view.WindowInsetsCompat;
+
+    public class SecretActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+            setContentView(R.layout.activity_secret);
+
+            Intent recievedntent = getIntent();
+            String sharedText = recievedntent.getStringExtra(Intent.EXTRA_TEXT);
+            if(sharedText != null){
+                TextView debugText = findViewById(R.id.debug_text);
+                debugText.setText("Shared: " + sharedText);
+            }
+        }
+    }
